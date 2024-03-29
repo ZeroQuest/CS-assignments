381aa4e59438f730fc7634a79bb1aced3d1cf613
user <trsydx@umsystem.edu>
2024-03-28 21:22:58 -0400

    hmm

--- Intro_to_AI/ai_hw2_refactor/hw2_refactor.cpp
+++ Intro_to_AI/ai_hw2_refactor/hw2_refactor.cpp
@@ -1,8 +1,12 @@
+#include <cstdio>
 #include <iostream>
 #include <limits>
 #include <string>
+#include <unordered_map>
+#include <utility>
 #include <vector>
 #include <fstream>
+#include <algorithm>
 
 struct Monster
 {
@@ -20,6 +24,7 @@ struct GameState
   //char actman_sprite = 'A';
   bool hasActmanShot = false;
   bool isActmanAlive = false;
+  bool victory = false;
   int score;
   std::vector<Monster> monster_list = {};
   std::string actionList = "";
@@ -28,6 +33,28 @@ struct GameState
   //unsigned int grid_col = 1;
   std::vector<std::vector<char>> grid;
 };
+
+using ActionMap = std::unordered_map<char, std::pair<int, int>>;
+
+ActionMap createActionMap()
+{
+  ActionMap actions;
+
+  actions['1'] = std::make_pair(1, -1);
+  actions['2'] = std::make_pair(1, 0);
+  actions['3'] = std::make_pair(1, 1);
+  actions['4'] = std::make_pair(0, -1);
+  actions['6'] = std::make_pair(0, 1);
+  actions['7'] = std::make_pair(-1, -1);
+  actions['8'] = std::make_pair(-1, 0);
+  actions['9'] = std::make_pair(-1, 1);
+  actions['N'] = std::make_pair(-1, 0);
+  actions['S'] = std::make_pair(1, 0);
+  actions['E'] = std::make_pair(0, 1);
+  actions['W'] = std::make_pair(0, -1);
+
+  return actions;
+}
 
 //IO Functions
 void readFromFile(const std::string& filename, GameState& state)
@@ -61,7 +88,8 @@ void readFromFile(const std::string& filename, GameState& state)
       else if(line[k] == 'D')
         state.monster_list.push_back(Monster(i, k, 'D'));
         
-      state.grid[i][k] = (line[k] == '#' || line[k] == ' ' || line[k] == '@') ? line[k] : ' ';
+      //state.grid[i][k] = (line[k] == '#' || line[k] == ' ' || line[k] == '@') ? line[k] : ' ';
+      state.grid[i][k] = line[k];
     }
   }
   file.close();
@@ -82,6 +110,126 @@ std::string writeToString(const GameState& state)
   return map;
 }
 
+//Move Validation
+bool validMove(char action, const GameState& state)
+{
+  ActionMap actions = createActionMap();
+
+  int newRow = state.actman_row;
+  int newCol = state.actman_col;
+
+  auto it = actions.find(action);
+
+  if(it != actions.end() && (action >= '1' && action <= '9'))
+  {
+    newRow += it->second.first;
+    newCol += it->second.second;
+
+    if(!(state.grid[newRow][newCol] == '#'))
+    {
+      std::cout << "Valid move!" << std::endl;
+      return true;
+    }
+    else
+    {
+      std::cout << "Invalid move!" << std::endl;
+      return false;
+    }
+  }
+  else if(it != actions.end() && (action == 'N' || action == 'S' | action == 'E' | action == 'W') && state.hasActmanShot == false)
+  {
+    std::cout << "Valid Move!" << std::endl;
+    return true;
+  }
+  else
+  {
+    std::cout << "Invalid Move!" << std::endl;
+    return false;
+  }
+
+  //return false;
+}
+
+void actmanShoot(GameState& state, ActionMap& actions, char direction)
+{
+  int currentRow = state.actman_row;
+  int currentCol = state.actman_col;
+
+  std::vector<Monster> newList = {};
+
+  auto it = actions.find(direction);
+
+  while(it != actions.end() && state.grid[currentRow][currentCol] != '#')
+  {
+    if(state.grid[currentRow][currentCol] == 'G' || state.grid[currentRow][currentCol] == 'D')
+    {
+      for(auto monsterIt = state.monster_list.begin(); monsterIt != state.monster_list.end(); ++monsterIt)
+      {
+        if(monsterIt->row_pos == currentRow && monsterIt->col_pos == currentCol)
+        {
+          state.grid[currentRow][currentCol] = '@';
+          //removalList.push_back(*monsterIt);
+          state.score += 5;
+        }
+        else {
+          newList.push_back(*monsterIt); 
+        }
+      }//end while
+    }
+    currentRow += it->second.first;
+    currentCol += it->second.second;
+  }//end while
+
+  //Cleanup monster_list
+  state.monster_list = newList;
+
+  state.hasActmanShot = true;
+  state.score = state.score - 20;
+}
+
+//Input Actions for testing!
+void actmanInputMove(GameState& state, char input)
+{
+  ActionMap actions = createActionMap();
+
+  int newRow = 0;
+  int newCol = 0;
+
+  auto it = actions.find(input);
+
+  if(validMove(input, state))
+  {
+    newRow = state.actman_row + it->second.first;
+    newCol = state.actman_col + it->second.second;
+
+    if(state.grid[newRow][newCol] == 'G' || state.grid[newRow][newCol] == 'D' || state.grid[newRow][newCol] == '@')
+    {
+      state.isActmanAlive = false;
+      state.grid[state.actman_row][state.actman_col] = ' ';
+      state.grid[newRow][newCol] = 'X';
+      state.actman_row = newRow;
+      state.actman_col = newCol;
+      state.score = 0;
+    }
+    else if(input == 'N' || input == 'S' || input == 'E' || input == 'W')
+    {
+      actmanShoot(state, actions, input);
+      state.actionList.push_back(it->first);
+    }
+    else 
+    {
+      state.grid[newRow][newCol] = 'A';
+      state.grid[state.actman_row][state.actman_col] = ' ';
+      state.actman_row = newRow;
+      state.actman_col = newCol;
+      state.score -= 1;
+      state.actionList.push_back(it->first);
+    }
+  }
+  else 
+    std::cout << "INVALID INPUT" << std::endl;
+}
+
 //end Functions
 
 int main(int argc, char* argv [])
@@ -99,6 +247,39 @@ int main(int argc, char* argv [])
 
   std::cout << gameBoard;
 
+  int count = 1;
+  char input;
+  bool acted = false;
+
+  std::cout << "Running" << std::endl;
+
+  while((state.isActmanAlive) && (count < 7) && !(state.victory))
+  {
+    std::cout << "Entered Loop" << std::endl;
+    while(acted == false)
+    {
+      std::cout << "Waiting for Input" << std::endl;
+      std::cin >> input;
+      if(validMove(input, state))
+      {
+        acted = true;
+      }
+      else {
+        input = ' ';
+      }
+    }
+
+    actmanInputMove(state, input);
+
+    gameBoard = writeToString(state);
+
+    std::cout << gameBoard;
+    
+    acted = false;
+    count++;
+  }
+  
+  //Write to File
   std::ofstream file(OUTPUT_FILENAME);
 
   if(!file.is_open())
